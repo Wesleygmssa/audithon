3c823939d9d4217f66ac15dbce7f97ddf828df71
Wesley Guerra <guerra_ssa@hotmail.com>
2021-03-20 16:43:18 -0300

    Importando os components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { Banner } from "./components/Banner";
+import { Header } from "./components/Header";
+
 function App() {
-  return <div>Teste</div>;
+  return (
+    <>
+      <Header />
+      <Banner />
+    </>
+  );
 }
 
 export default App;
