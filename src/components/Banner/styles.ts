b1a521fc6749ee9983309b4e4f315b43e14a2fe5
Wesley Guerra <guerra_ssa@hotmail.com>
2021-03-20 17:35:26 -0300

    Add background

--- src/components/Banner/styles.ts
+++ src/components/Banner/styles.ts
@@ -1,32 +1,28 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  position: relative;
 
-    position: relative;
-    img{
-        filter: brightness(0.4);
+  div {
+    width: 100%;
+    max-width: 1440px;
+    margin: 0 auto;
+    position: absolute;
+    text-align: center;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    border: 1px solid red;
+    h1 {
+      font-size: 3.8rem;
+      color: white;
+      letter-spacing: 0.1rem;
+      line-height: 6rem;
     }
-
-    div {
-        width: 100%;
-        max-width: 1440px;
-        margin: 0 auto;
-        position: absolute;
-        text-align: center;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        h1{
-            font-size: 3.8rem;
-            color: white;
-            letter-spacing: 0.1rem;
-            line-height: 6rem;
-        }
-        h3{
-            font-size: 1.5rem;
-            color: rgba(256,256,256, 0.8);
-            letter-spacing: 0.1rem;
-        }
+    h3 {
+      font-size: 1.5rem;
+      color: rgba(256, 256, 256, 0.8);
+      letter-spacing: 0.1rem;
     }
-
+  }
 `;
